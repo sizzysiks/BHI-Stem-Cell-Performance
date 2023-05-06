4d75dd3e21c3504fb650f74a0b5345fe13a3b1fc
theeseus <info@hiveai.tech>
2023-05-06 10:24:11 +0100

    updates to styling of bot

--- js/gpt.js
+++ js/gpt.js
@@ -1,5 +1,5 @@
 const user = ` 
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" class="bi bi-person" viewBox="0 0 16 16">
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffffff" class="bi bi-person" viewBox="0 0 16 16">
   <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
 </svg> `;
 
@@ -52,16 +52,31 @@ function generateUniqueId() {
     return `id-${timestamp}-${hexadecimalString}`;
 }
 
+// function chatStripe(isAi, value, uniqueId) {
+//     return (
+//         `
+//         <div class="wrapper ${isAi && 'ai'}">
+//             <div class="chat">
+//                 <div class="profile">
+//                     <img 
+//                       src=${isAi ? bot : user} 
+//                       alt="${isAi ? 'bot' : 'user'}" 
+//                     />
+//                 </div>
+//                 <div class="message" id=${uniqueId}>${value}</div>
+//             </div>
+//         </div>
+//     `
+//     )
+// }
+
 function chatStripe(isAi, value, uniqueId) {
     return (
         `
-        <div class="wrapper ${isAi && 'ai'}">
+        <div class="wrapper ${isAi ? 'ai' : ''}">
             <div class="chat">
                 <div class="profile">
-                    <img 
-                      src=${isAi ? bot : user} 
-                      alt="${isAi ? 'bot' : 'user'}" 
-                    />
+                    ${isAi ? bot : user}
                 </div>
                 <div class="message" id=${uniqueId}>${value}</div>
             </div>
@@ -69,6 +84,7 @@ function chatStripe(isAi, value, uniqueId) {
     `
     )
 }
+
 
 const handleSubmit = async (e) => {
     e.preventDefault();
